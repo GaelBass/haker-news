3eeecaa9b824980d808ea5a869559170663f08dd
GaelBass <bassonggael@gmail.com>
2023-07-03 15:51:08 +0200

    search bar adding js

---
+++ scripts.js
@@ -0,0 +1,22 @@
+const searchButton = document.getElementById('searchButton');
+searchButton.
+search
+addEventListener('click', performSearch);
+
+function performSearch() {
+  
+ 
+const searchInput = document.getElementById('searchInput').value;
+  
+ 
+
+  
+ 
+console.log('Recherche effectuée : ' + searchInput);
+}
+
+
+
+
+
+
